a002027dff2de2b6acfb6ce0dc9d99e781c83a36
Edwin Fuentes <efuentesamin@gmail.com>
2021-02-16 11:35:17 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState(null);
+    
+    useEffect(() => {
+        const endpoint = process.env.REACT_APP_ENDPOINT ||
+        'http://localhost:3000/welcome/';
+        setLoading(true);
+        fetch(endpoint)
+            .then(res => res.json())
+            .then(
+                result => {
+                    console.log('API Response: ', result);
+                    setLoading(false);
+                    setData(result.datetime);
+                },
+                e => {
+                    console.log('API Error: ', e);
+                    setLoading(false);
+                    setError(e);
+                }
+            )
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                {loading && <p>Cargando...</p>}
+                {error && <p>Ocurrió un error!</p>}
+                <p>La fecha del servidor es: {data}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
